c51364dd035ddf20c883d89ab0791f28a1db2aec
Prashant Ranjan <prasranjn@deloitte.com>
2024-06-03 15:25:26 +0530

    setting up tailwind and added footer and header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-100 justify-center py-4 flex'>
+        <div className='text-lg tracking-widest'>
+            Shop Cart
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-gray-100 flex justify-between px-14 py-4'>
+        <div className='text-lg tracking-widest'>
+            Shop Cart
+        </div>
+        <div>
+            Options
+        </div>
+    </div>
+  )
+}
+
+export default Header
